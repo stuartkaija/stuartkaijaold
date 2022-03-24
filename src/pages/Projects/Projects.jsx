07ba6d30477ef053994f5478037ee0863809245f
Stuart Kaija <stuartkaija@gmail.com>
2022-03-24 12:05:42 -0700

    project screenshots laid out on page

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import BubbleBuddies from '../../assets/photos/projects/bubble_buddies.png';
+import WhoopWork from '../../assets/photos/projects/whoop_work.png';
+import InStock from '../../assets/photos/projects/InStock.png';
 import './Projects.scss';
 
 export default function Projects() {
-  return (
-    <div className='projects'>
-        <p className='projects__words'>This is the projects page!</p>
-    </div>
-  )
+    return (
+        <div className='projects'>
+
+            {/* <div className='projects__image-container'> */}
+                <img className='projects__image' src={BubbleBuddies} alt="Bubble Buddies" />
+            {/* </div> */}
+
+            {/* <div className='projects__image-container'> */}
+                <img className='projects__image'  src={WhoopWork} alt="WhoopWork" />
+            {/* </div> */}
+
+            {/* <div className='projects__image-container'> */}
+                <img className='projects__image'  src={InStock} alt="InStock" />
+            {/* </div> */}
+
+        </div>
+    )
 }
