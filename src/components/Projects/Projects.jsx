1403ea0d13e609911515513ffa3b4bd586501125
Stuart Kaija <stuartkaija@gmail.com>
2022-03-25 17:27:16 -0700

    dynamic skills list added to resume component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,15 +5,15 @@ export default function Projects() {
   return (
 		<div id='projects' className='projects'>
 			<ul className='projects__list'>
-				<li>
+				<li className='projects__list-item'>
 					<a className='projects__link' href="https://github.com/stuartkaija/bubble-buddies" target='_blank' rel='noreferrer'>Bubble Buddies</a>
 					<p className='projects__words'>React, Sass, Node, Express - blah blah blah</p>
 				</li>
-				<li>
+				<li className='projects__list-item'>
 					<a className='projects__link' href="">WhoopWork</a>
 					<p className='projects__words'>Industry hack-a-thon - with a team of two designers and two developers, ideated and brought to life a new feature for wearable fitness brand Whoop.</p>
 				</li>
-				<li>
+				<li className='projects__list-item'>
 					<a className='projects__link' href="https://github.com/stuartkaija/instock" target='_blank' rel='noreferrer'>InStock</a>
 					<p className='projects__words'>A warehouse inventory application created with a team of 3 other developers. Successfully utilized agile methodologies, Jira, and GitHub version control to collaborate</p>
 				</li>
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 import './Resume.scss';
 
 export default function Resume() {
+
+    const skills = ['HTML', 'CSS/SCSS', 'JavaScript', 'React', 'Node', 'Express', 'Agile Development', 'Web APIs', 'Responsive Design', 'Web Accessibility']
+
     return (
-        <div id='resume' className='resume'>Resume (and list skills?)</div>
+        <div id='resume' className='resume'>
+            <h1 className='resume__header'>Experience with:</h1>
+            <ul>
+                {skills.map((skill) => {
+                    return (
+                        <li className='resume__list-item'>{skill}</li>
+                    )
+                })}
+            </ul>
+        </div>
     )
 }
